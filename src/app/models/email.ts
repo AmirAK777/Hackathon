ff1bed9901ba51289a92883328ae0a3980a90a94
AmirAK777 <contactezpro@gmail.com>
2024-01-30 14:49:53 +0100

    commit

--- src/app/models/email.ts
+++ src/app/models/email.ts
@@ -1,22 +1,27 @@
 export class Email {
-    id: number;
-    expediteur: string;
-    destinataire: string;
-    objet: string;
-    contenu: string;
-    lienDuSite: string;
-    imageEntreprise: string;
-    scams: boolean;
-  
-    constructor(id: number, expediteur: string, destinataire: string, objet: string, contenu: string, lienDuSite: string, imageEntreprise: string, scams: boolean) {
-      this.id = id;
-      this.expediteur = expediteur;
-      this.destinataire = destinataire;
-      this.objet = objet;
-      this.contenu = contenu;
-      this.lienDuSite = lienDuSite;
-      this.imageEntreprise = imageEntreprise;
-      this.scams = scams;
-    }
+  id: number;
+  expediteur: string;
+  destinataire: string;
+  objet: string;
+  contenu: string;
+  lienDuSite: string;
+  imageEntreprise: string;
+  scams: boolean;
+  heure: string;
+  nomExpediteur: string;
+  pieceJointe: string;
+
+  constructor(id: number, expediteur: string, destinataire: string, objet: string, contenu: string, lienDuSite: string, imageEntreprise: string, scams: boolean, heure: string, nomExpediteur: string, pieceJointe: string) {
+    this.id = id;
+    this.expediteur = expediteur;
+    this.destinataire = destinataire;
+    this.objet = objet;
+    this.contenu = contenu;
+    this.lienDuSite = lienDuSite;
+    this.imageEntreprise = imageEntreprise;
+    this.scams = scams;
+    this.heure = heure;
+    this.nomExpediteur = nomExpediteur;
+    this.pieceJointe = pieceJointe;
   }
-  
+}
